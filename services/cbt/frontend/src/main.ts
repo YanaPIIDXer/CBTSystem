4f3a0b40320c5f8da7004fef0c947a85ff201345
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-19 10:52:29 +0900

    PrimeVueの準備

--- services/cbt/frontend/src/main.ts
+++ services/cbt/frontend/src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.mount("#app");
